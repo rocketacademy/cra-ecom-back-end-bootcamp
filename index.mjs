045ae5937e245abb6ab73e866c5639879b6efc32
awongh <akira@awongh.com>
2021-02-08 00:46:54 +0800

    fix index.mjs, add CORS

--- index.mjs
+++ index.mjs
@@ -1,15 +1,13 @@
 import cookieParser from 'cookie-parser';
 import express from 'express';
 import methodOverride from 'method-override';
-import webpack from 'webpack';
-import webpackDevMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
-
-import webpackConfig from './webpack_conf/webpack.dev.js';
 import bindRoutes from './routes.mjs';
+import cors from 'cors';
 
 // Initialise Express instance
 const app = express();
+// Set CORS headers
+app.use(cors());
 // Set the Express view engine to expect EJS templates
 app.set('view engine', 'ejs');
 // Bind cookie parser middleware to parse cookies in requests
@@ -22,24 +20,6 @@ app.use(express.json());
 app.use(methodOverride('_method'));
 // Expose the files stored in the public folder
 app.use(express.static('public'));
-// Expose the files stored in the distribution folder
-app.use(express.static('dist'));
-
-// Set up Webpack in dev env
-const env = process.env.NODE_ENV || 'development';
-if (env === 'development') {
-  const compiler = webpack(webpackConfig);
-  app.use(webpackDevMiddleware(compiler, {
-    publicPath: webpackConfig.output.publicPath,
-    // html only
-    writeToDisk: (filePath) => /\.html$/.test(filePath),
-  }));
-  app.use(webpackHotMiddleware(compiler, {
-    log: false,
-    path: '/__webpack_hmr',
-    heartbeat: 10 * 1000,
-  }));
-}
 
 // Bind route definitions to the Express application
 bindRoutes(app);

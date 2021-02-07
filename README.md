54d57666a9019768c65dfefaebabd3fcd9f8b513
awongh <akira@awongh.com>
2021-02-08 01:33:48 +0800

    change db details for this app

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # cra-ecom-back-end-swe1
+
+```
+npx sequelize db:create
+npx sequelize db:migrate
+npx sequelize db:seed:all
+```
--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     username: '<YOUR_USER_NAME>',
     password: null,
-    database: 'react_ecom_development',
+    database: 'cra_ecom_development',
     host: '127.0.0.1',
     dialect: 'postgres',
   },

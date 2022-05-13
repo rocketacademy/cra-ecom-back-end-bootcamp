b7f86c3e65bf3fbcbe54e3593d87fb593014e5d0
neokaiyuan <kai@rocketacademy.co>
2022-05-13 17:32:23 +0800

    lint

--- index.mjs
+++ index.mjs
@@ -1,20 +1,22 @@
-import cookieParser from 'cookie-parser';
-import express from 'express';
-import methodOverride from 'method-override';
-import bindRoutes from './routes.mjs';
-import cors from 'cors';
+import cookieParser from "cookie-parser";
+import express from "express";
+import methodOverride from "method-override";
+import bindRoutes from "./routes.mjs";
+import cors from "cors";
 
-const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
 
 // Initialise Express instance
 const app = express();
 // Set CORS headers
-app.use(cors({
-  credentials:true,
-  origin: FRONTEND_URL
-}));
+app.use(
+  cors({
+    credentials: true,
+    origin: FRONTEND_URL,
+  })
+);
 // Set the Express view engine to expect EJS templates
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 // Bind cookie parser middleware to parse cookies in requests
 app.use(cookieParser());
 // Bind Express middleware to parse request bodies for POST requests
@@ -22,9 +24,9 @@ app.use(express.urlencoded({ extended: false }));
 // Bind Express middleware to parse JSON request bodies
 app.use(express.json());
 // Bind method override middleware to parse PUT and DELETE requests sent as POST requests
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 // Expose the files stored in the public folder
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Bind route definitions to the Express application
 bindRoutes(app);
